5a8697df016f32133e05cb9f70a61e99df17b697
OGogte <87823041+OGogte@users.noreply.github.com>
2023-10-11 02:45:27 +0530

    Time: 16 ms (6.29%), Space: 13 MB (6.01%) - LeetHub

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<string> s;
+        int solution = 0;
+        for(int i = 0; i < tokens.size(); ++i) {
+            if (tokens[i] == "+" || tokens[i] == "-" || tokens[i] == "*" || tokens[i] == "/") {
+                string s1 = s.top();
+                s.pop();
+                string s2 = s.top();
+                s.pop();
+                int i1 = stoi(s1);
+                int i2 = stoi(s2);
+                if (tokens[i] == "+") {
+                    solution = (i1 + i2);
+                } else if (tokens[i] == "-") {
+                    solution = (i2 - i1);
+                } else if (tokens[i] == "/") {
+                    solution = (i2 / i1);
+                } else if (tokens[i] == "*") {
+                    solution = (i2 * i1);
+                }
+                s.push(to_string(solution));
+            } else {
+                string w1 = tokens[i];
+                int a = stoi(w1);
+                s.push(to_string(a));
+            }
+        }
+        return stoi(s.top());
+    }
+};
