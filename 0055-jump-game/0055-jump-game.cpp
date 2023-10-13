c045f3f6722f47987405a0f1b6a50d6db9adaccb
OGogte <87823041+OGogte@users.noreply.github.com>
2023-10-14 01:29:57 +0530

    Time: 50 ms (49.87%), Space: 48.7 MB (52.77%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int max_jump = 0;
+        // int max_jump = 0;
         for(int i=0;i<nums.size();++i)
         {
-            if(max_jump<i)
+            if(nums[0]<i)
             {
                 return 0;
             }
-            max_jump=max(max_jump,i+nums[i]);
+            nums[0]=max(nums[0],i+nums[i]);
         }
         return 1;
     }
